ecb4c37acc519b7d56f4d3561113e54914a7075d
rafael-sousa <rafaelsj7@gmail.com>
2021-03-07 09:51:24 -0300

    feat: increase the list of handled error

--- pkg/model/types/err.go
+++ pkg/model/types/err.go
@@ -11,12 +11,16 @@ type ErrCode string
 
 // List of common application errors
 const (
-	InternalErr      ErrCode = "0000" // InternalErr represents an unpredicted error such as db connection or network failure
-	EmptyResultErr   ErrCode = "0010" // EmptyResultErr represents a state where a given query returned no rows
-	SelectStmtErr    ErrCode = "0020" // SelectStmtErr represents a state where a select stmt completed unsuccessfully
-	InsertStmtErr    ErrCode = "0030" // InsertStmtErr represents a state where a insert stmt completed unsuccessfully
-	UpdateStmtErr    ErrCode = "0040" // UpdateStmtErr represents a state where a update stmt completed unsuccessfully
-	NoRowAffectedErr ErrCode = "0050" // NoRowAffectedErr represents a state where a update haven't affect a single row
+	InternalErr       ErrCode = "0000" // InternalErr represents an unpredicted error such as db connection or network failure
+	EmptyResultErr    ErrCode = "0010" // EmptyResultErr represents a state where a given query returned no rows
+	SelectStmtErr     ErrCode = "0020" // SelectStmtErr represents a state where a select stmt completed unsuccessfully
+	InsertStmtErr     ErrCode = "0030" // InsertStmtErr represents a state where a insert stmt completed unsuccessfully
+	UpdateStmtErr     ErrCode = "0040" // UpdateStmtErr represents a state where a update stmt completed unsuccessfully
+	NoRowAffectedErr  ErrCode = "0050" // NoRowAffectedErr represents a state where a update haven't affect a single row
+	ValidationErr     ErrCode = "0060" // ValidationErr represents a business rule violation
+	NotFoundErr       ErrCode = "0070" // NotFoundErr occurs when accessing a nonexisting resource
+	AuthenticationErr ErrCode = "0080" // AuthenticationErr occurs when the authentication process completes unsuccessfully
+	ConflictErr       ErrCode = "0090" // ConflictErr occurs an operation could not complete due to a conflict with the current state of the resource
 )
 
 // Err represents an error acknowledged by the application business

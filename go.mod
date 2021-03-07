d0918601279e94f2851a57d9bfacde41f19ac8ba
rafael-sousa <rafaelsj7@gmail.com>
2021-03-07 19:20:05 -0300

    feat: include rest server abstraction

--- go.mod
+++ go.mod
@@ -4,9 +4,10 @@ go 1.15
 
 require (
 	github.com/containerd/containerd v1.4.4 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/docker v20.10.5+incompatible // indirect
-	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-migrate/migrate v3.5.4+incompatible
@@ -15,6 +16,7 @@ require (
 	github.com/ory/dockertest/v3 v3.6.3
 	github.com/rs/zerolog v1.20.0
 	github.com/sethvargo/go-envconfig v0.3.2
+	github.com/swaggo/http-swagger v1.0.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
 	google.golang.org/grpc v1.36.0 // indirect
---
+++ pkg/controller/rest/server.go
@@ -0,0 +1,55 @@
+// Package rest and its sub-packages are the application REST entry point.
+package rest
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi"
+	"github.com/rafael-sousa/stn-accounts/pkg/controller/rest/routing"
+	"github.com/rafael-sousa/stn-accounts/pkg/model/env"
+	"github.com/rafael-sousa/stn-accounts/pkg/service"
+	httpSwagger "github.com/swaggo/http-swagger"
+)
+
+type server struct {
+	accountServ  *service.Account
+	transferServ *service.Transfer
+	middlewares  []func(http.Handler) http.Handler
+}
+
+// Server exposes the services provived by the application via REST
+type Server interface {
+	Use(m ...func(http.Handler) http.Handler) *server
+	Start(c *env.RestConfig) error
+}
+
+// Use appends the given middleware(s) to the server's middleware slice.
+func (s *server) Use(m ...func(http.Handler) http.Handler) *server {
+	s.middlewares = append(s.middlewares, m...)
+	return s
+}
+
+// Start kicks off the service by registering the application routes and applying the configured middlewares
+func (s *server) Start(cfg *env.RestConfig) error {
+
+	r := chi.NewRouter()
+	for _, md := range s.middlewares {
+		r.Use(md)
+	}
+	r.Route("/accounts", routing.Accounts(s.accountServ))
+	r.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL("doc.json")))
+	if err := http.ListenAndServe(":"+strconv.Itoa(cfg.Port), r); err != nil {
+		return fmt.Errorf("Failed to start and listen the http server at port %d, %v", cfg.Port, err)
+	}
+	return nil
+}
+
+// NewServer constructs a server with its required dependencies
+func NewServer(accountServ *service.Account, transferServ *service.Transfer) Server {
+	return &server{
+		accountServ:  accountServ,
+		transferServ: transferServ,
+	}
+}

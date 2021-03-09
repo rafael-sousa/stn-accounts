4274636afe8603de8cbb00e08a04c244568d707f
rafael-sousa <rafaelsj7@gmail.com>
2021-03-09 17:37:41 -0300

    refactor: simplify response controller artefacts

---
+++ pkg/controller/rest/response/writer.go
@@ -0,0 +1,68 @@
+package response
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/rafael-sousa/stn-accounts/pkg/controller/rest/body"
+	"github.com/rafael-sousa/stn-accounts/pkg/model/types"
+)
+
+func appendHeaders(header http.Header) {
+	header.Set("Content-Type", "application/json")
+	header.Set("Accept", "application/json")
+	header.Set("Accept-Charset", "utf-8")
+}
+
+// WriteErr writes an appropriate error message into the http response.
+// It'll write a respose status code from either 4xx or 5xx category
+func WriteErr(w http.ResponseWriter, r *http.Request, err error) error {
+	status, errCode := http.StatusInternalServerError, types.InternalErr
+	if err, ok := err.(*types.Err); ok {
+		errCode = err.Code
+		switch err.Code {
+		case types.NotFoundErr:
+			status = http.StatusNotFound
+		case types.EmptyResultErr:
+			status = http.StatusNotFound
+		case types.ValidationErr:
+			status = http.StatusBadRequest
+		case types.AuthenticationErr:
+			status = http.StatusUnauthorized
+		case types.ConflictErr:
+			status = http.StatusConflict
+		}
+	}
+
+	appendHeaders(w.Header())
+	w.WriteHeader(status)
+	msg := err.Error()
+	if appErr, ok := err.(*types.Err); ok {
+		msg = appErr.Msg
+	}
+	return json.NewEncoder(w).Encode(&body.JSONError{
+		Name:    http.StatusText(status),
+		Code:    string(errCode),
+		Time:    time.Now(),
+		Message: msg,
+		Path:    r.URL.RequestURI(),
+	})
+}
+
+// WriteSuccess writes to the response with appropriate status and headers
+func WriteSuccess(w http.ResponseWriter, r *http.Request, b interface{}, id interface{}) error {
+	appendHeaders(w.Header())
+	if b == nil {
+		w.WriteHeader(http.StatusNoContent)
+		return nil
+	}
+	status := http.StatusOK
+	if id != nil {
+		status = http.StatusCreated
+		w.Header().Set("Location", fmt.Sprintf("%s/%v", r.URL.RequestURI(), id))
+	}
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(b)
+}
